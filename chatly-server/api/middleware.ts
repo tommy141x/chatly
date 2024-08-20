19088e322a2a5e84cc815ff431d9d5a018720463
tommy <t.johnston141x@gmail.com>
2024-08-20 03:27:53 -0400

    Update env stuff

--- chatly-server/api/middleware.ts
+++ chatly-server/api/middleware.ts
@@ -1,12 +1,16 @@
 export default function middleware(req, res, next) {
-  // Set CORS headers to allow all origins
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
+  if (process.env.DEV_MODE) {
+    // Set CORS headers to allow all origins
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization",
+    );
+  }
   // Handle preflight requests
   if (req.method === "OPTIONS") {
     res.status(200).end();
--- chatly-server/index.ts
+++ chatly-server/index.ts
@@ -9,6 +9,6 @@ await wipeDB();
 await initDB();
 initRoutes(app);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port " + process.env.PORT);
 });
