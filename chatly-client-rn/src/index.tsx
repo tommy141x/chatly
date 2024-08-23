51087d27d925c8c71462cae8083ada23c06a9095
tommy <t.johnston141x@gmail.com>
2024-08-22 21:52:14 -0400

    Add gluestack ui

--- chatly-client-rn/src/index.tsx
+++ chatly-client-rn/src/index.tsx
@@ -1,15 +1,64 @@
+import * as React from "react";
 import { StatusBar } from "expo-status-bar";
+import {
+  AlertDialog,
+  AlertDialogBackdrop,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogCloseButton,
+  AlertDialogFooter,
+  AlertDialogBody,
+} from "@/components/ui/alert-dialog";
+import { Heading } from "@/components/ui/heading";
+import { Button, ButtonText } from "@/components/ui/button";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View } from "react-native";
 import "@/styles/global.css";
 
 export default function App() {
+  const [showAlertDialog, setShowAlertDialog] = React.useState(false);
+  const handleClose = () => setShowAlertDialog(false);
   return (
-    <View style={styles.container}>
-      <Text className="text-green-500">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+      <View style={styles.container}>
+        <Text className="text-green-500">
+          Open up App.js to start working on your app!
+        </Text>
+        <Button onPress={() => setShowAlertDialog(true)}>
+          <ButtonText>Open Dialog</ButtonText>
+        </Button>
+        <AlertDialog isOpen={showAlertDialog} onClose={handleClose} size="md">
+          <AlertDialogBackdrop />
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <Heading className="text-typography-950 font-semibold" size="md">
+                Are you sure you want to delete this post?
+              </Heading>
+            </AlertDialogHeader>
+            <AlertDialogBody className="mt-3 mb-4">
+              <Text size="sm">
+                Deleting the post will remove it permanently and cannot be
+                undone. Please confirm if you want to proceed.
+              </Text>
+            </AlertDialogBody>
+            <AlertDialogFooter className="">
+              <Button
+                variant="outline"
+                action="secondary"
+                onPress={handleClose}
+                size="sm"
+              >
+                <ButtonText>Cancel</ButtonText>
+              </Button>
+              <Button size="sm" onPress={handleClose}>
+                <ButtonText>Delete</ButtonText>
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
   );
 }
 
