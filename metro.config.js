191e71ce04802adc77f5374716ecd7a8ffa3f6df
tommy141x <tommy@timmygstudios.com>
2024-11-09 23:54:52 -0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,9 +4,4 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// --- burnt ---
-config.resolver.sourceExts.push("mjs");
-config.resolver.sourceExts.push("cjs");
-// --- end burnt ---
-
 module.exports = withNativeWind(config, { input: "./global.css" });
